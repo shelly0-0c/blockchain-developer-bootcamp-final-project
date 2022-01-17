d29515e3152c134a87c548a4d97844db18e5552a
shelly <shelly@Shellys-MacBook-Pro.local>
2022-01-18 01:37:53 +0800

    update contracts deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,7 +1,7 @@
 var RewardEscrow = artifacts.require("./RewardEscrow.sol");
 var SurveyProcessor = artifacts.require("./SurveyProcessor.sol");
 
-module.exports = (deployer, network, accounts) => {
+module.exports = (deployer) => {
   deployer.deploy(RewardEscrow).then(() => {
     return deployer.deploy(SurveyProcessor, RewardEscrow.address);
   });
