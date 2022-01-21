fd3083da14d4784028833bd0e8cf59e150377110
shelly <shelly@Shellys-MacBook-Pro.local>
2022-01-21 23:48:07 +0800

    test update

--- test/RewardEscrow.test.js
+++ test/RewardEscrow.test.js
@@ -1,9 +1,5 @@
 let { constants, expectEvent } = require("@openzeppelin/test-helpers");
 
-const {
-  items,
-} = require("../../supply-chain-exercise-shelly0-0c/test/ast-helper");
-
 let RewardEscrow = artifacts.require("RewardEscrow");
 
 contract("RewardEscrow", (accounts) => {
@@ -14,17 +10,8 @@ contract("RewardEscrow", (accounts) => {
 
   before(async () => {
     reInstance = await RewardEscrow.new();
-    reInstance.setAgent(surveyProcessorContract);
   });
 
-  describe("Reward Pool Functionalities", () => {
-    it("should have an agent address set", async () => {
-      let agent = reInstance.getAgent();
-      assert.equal(
-        agent,
-        surveyProcessorContract,
-        "Agent address is not set correctly."
-      );
-    });
-  });
+  // TODO
+  // describe("Reward Pool Functionalities", () => {});
 });
